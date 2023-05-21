543a69d1f5ac653c388fc73513cac6d27c4cd81f
John Nagy <aethorion@gmail.com>
2023-05-20 21:55:25 -0400

    fdbsauifndas

--- components/mintNft.tsx
+++ components/mintNft.tsx
@@ -8,14 +8,10 @@ interface MintNftProps {
 }
 interface Attribute {
   trait_type: string;
-  value: string | number;
-  key: string;
+  value: string;
+  key?: string;
 }
 
-interface AttributeInput {
-  trait_type: string;
-  value: string;
-}
 
 async function toMetaplexFileFromBrowser(file: File): Promise<MetaplexFile> {
   const ext = file.type.split("/").pop();
@@ -33,11 +29,33 @@ function MintNft({ session }: MintNftProps) {
   const connection = new Connection(clusterApiUrl('devnet'));
   const wallet = useWallet();
   const [nftMinted, setNftMinted] = useState(false);
-
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [image, setImage] = useState<File | undefined>(undefined);
 
+//declares and sets attributes based on the Attribute interface and allows the users to set the value trait type and key
+  const [attributes, setAttributes] = useState<Attribute[]>([
+    {
+      trait_type: '',
+      value: '',
+      key: '',
+    }]);
+//allows the user to add more attributes
+  const addAttribute = () => {
+    setAttributes([...attributes, {
+      trait_type: '',
+      value: '',
+      key: '',
+    }]);
+  };
+//allows the user to remove attributes
+  const removeAttribute = (index: number) => {
+    const newAttributes = [...attributes];
+    newAttributes.splice(index, 1);
+    setAttributes(newAttributes);
+  };
+  
+    
 
   const handleMintNft = async () => {
     const mx = Metaplex.make(connection)
@@ -53,10 +71,15 @@ function MintNft({ session }: MintNftProps) {
       return;
     }
 
+    const formattedAttributes = attributes.map((attribute) => ({
+      trait_type: attribute.trait_type,
+      value: attribute.value,
+    }));
+
     const { uri, metadata } = await mx.nfts().uploadMetadata({
         name: name,
         description: description,
-        attributes:attributes,
+        attributes:formattedAttributes,
         image: await toMetaplexFileFromBrowser(image),
       });
       
@@ -89,6 +112,37 @@ function MintNft({ session }: MintNftProps) {
           Image:
           <input type="file" onChange={(e) => setImage(e.target.files?.[0])} />
         </label>
+        {attributes.map((attribute, index) => (
+          <div key={index}>
+            <label>
+              Trait Type:
+              <input type="text" value={attribute.trait_type} onChange={(e) => {
+                const newAttributes = [...attributes];
+                newAttributes[index].trait_type = e.target.value;
+                setAttributes(newAttributes);
+              }} />
+            </label>
+            <label>
+              Value:
+              <input type="text" value={attribute.value} onChange={(e) => {
+                const newAttributes = [...attributes];
+                newAttributes[index].value = e.target.value;
+                setAttributes(newAttributes);
+              }} />
+            </label>
+            <label>
+              Key:
+              <input type="text" value={attribute.key} onChange={(e) => {
+                const newAttributes = [...attributes];
+                newAttributes[index].key = e.target.value;
+                setAttributes(newAttributes);
+              }} />
+            </label>
+            <button onClick={() => removeAttribute(index)}>Remove</button>
+          </div>
+        ))}
+        <button onClick={addAttribute}>Add Attribute</button>
+
         
       {session ? (
         <div>
