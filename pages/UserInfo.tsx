976b74bc2049f9a351bfe57581ad86a9b1a32a2e
John Nagy <aethorion@gmail.com>
2023-05-23 16:23:29 -0400

    acc stuff?

---
+++ pages/UserInfo.tsx
@@ -0,0 +1,26 @@
+import { SessionProvider, GetSessionParams, getSession } from "next-auth/react"
+import AccSetup from "../components/accSetup"
+import Layout from "../components/layout"
+
+interface CustomMintPageProps {
+  session: any
+}
+
+export default function CustomMintPage({ session }: CustomMintPageProps) {
+  return (
+    <Layout>
+      <SessionProvider session={session}>
+        <AccSetup session={session} />
+      </SessionProvider>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps(context: GetSessionParams | undefined) {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  }
+}
+
